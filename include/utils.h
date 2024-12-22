e783b08cc86c1fc661be2d953acbfe148ede36cf
Ray5mz <136928230+Ray5mz@users.noreply.github.com>
2024-12-22 19:18:24 +0100

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,1 +1,11 @@
-sa
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <SDL2/SDL.h>
+#include "cgame.h"
+#include "render.h"
+#define GRID_SIZE 4
+
+// Function to fill the grid with random "2" values and render them
+int add_random_tile(Game* game);
+#endif // UTILS_H
