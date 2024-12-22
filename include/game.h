f051f4c2e5ec3cd488263e310ad305469b5311dd
Ray5mz <136928230+Ray5mz@users.noreply.github.com>
2024-12-22 19:14:54 +0100

    Update game.h

--- include/game.h
+++ include/game.h
@@ -2,24 +2,47 @@
 #define GAME_H
 
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
 #define GRID_SIZE 4
 #define TILE_SIZE 100
 #define GAP 10
+#define MAX_SCORES 5
+
+typedef enum {
+    A_NONE,
+    A_MOVE_RIGHT,
+    A_MOVE_LEFT,
+    A_MOVE_DOWN,
+    A_MOVE_UP,
+} Actions;
+
+typedef enum {
+    GS_PLAYING,
+    GS_LOST,
+    GS_WON,
+} GameStates;
 
 typedef struct {
-    int value;
-    SDL_Rect rect;
-    SDL_Color color;
-    int is_dragging;
-} Tile;
+    int board[4 * 4];
+    GameStates state;
+    Actions action;
+    int score, moves;
+    int last_inserted;
+    bool has_moved;
+} Game;
 
-extern SDL_Renderer* renderer; // Declare renderer
-extern Tile* dragging_tile;    // Declare dragging_tile (only declaration, not definition)
-extern Tile grid[GRID_SIZE][GRID_SIZE]; // Declare grid (only declaration, not definition)
+bool has_won(Game* game);
+bool has_lost(Game* game);
+int empty_tiles(Game* game);
+bool move_cell_maybe_break(Game* game, int* target, int* src);
+void move_right(Game* game);
+void move_left(Game* game);
+void move_up(Game* game);
+void move_down(Game* game);
+void update(Game* game, double delta);
+void render_thegrid(SDL_Renderer* renderer, int window_width, int window_height, Game* game);
+void initialize_game(Game* game);
 
-void render_rounded_rect(SDL_Renderer* renderer, SDL_Rect* rect, int radius);
-void initialize_game();
-void render_grid(SDL_Renderer* renderer, int window_width, int window_height);
 
-#endif
+#endif // GAME_H
