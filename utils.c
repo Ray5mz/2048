21af913de640c54784557adba5f526b48816ccdf
Ray5mz <136928230+Ray5mz@users.noreply.github.com>
2025-01-15 16:25:22 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include "include/cgame.h"
+#include "include/game.h"
 
 #define GRID_SIZE 4
 
@@ -13,6 +13,23 @@ int add_random_tile(Game* game) {
         if (game->board[i] == 0) {
             if (n == tile) {
                 game->board[i] = rand() > RAND_MAX / 3 * 2 ? 2 : 4;
+                return i;
+            } else {
+                n++;
+            }
+        }
+    }
+    return -1;
+}
+
+int add_random_tileM(Game* game) {
+    int tiles = empty_tiles(game);
+    int tile = ((rand() / 16) * tiles) / (RAND_MAX / 16);
+    int n = 0;
+    for (int i = 0; i < 16; i++) {
+        if (game->ai_board[i] == 0) {
+            if (n == tile) {
+                game->ai_board[i] = rand() > RAND_MAX / 3 * 2 ? 2 : 4;
                 return i;
             } else {
                 n++;
